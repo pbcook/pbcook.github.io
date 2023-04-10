f58646d132d020e99a4e8e24bac48c89c59e71a6
pbcook <91858545+pbcook@users.noreply.github.com>
2023-04-10 19:34:21 -0400

    feat: update to client project images

--- itis3135/client_project/default.js
+++ itis3135/client_project/default.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    const pathname = String(location.pathname.split("/").splice(-1));
-    if (pathname == "events.html") {
-        var map = L.map('map').setView([35.03156, -80.62019], 16);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        var market = L.marker([35.03159, -80.61997]).addTo(map);
-    }
-});    
+    console.log("Heyyyyyy");
+    var map = L.map('map').setView([35.03156, -80.62019], 16);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    var market = L.marker([35.03159, -80.61997]).addTo(map);
+});
